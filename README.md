93461d422c69f95263f763ae6e5754930bc230e8
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-07-18 12:49:24 +0100

    The actual initial commit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # Reward-Noises
- Simple listener which plays audio on specified reward redemptions
+
+Simple Twitch listener which plays audio on specified reward redemptions
+
+## Set-up
+
+ 1. Download latest release
+ 2. Download decencies from requirements.txt via pip
+ 3. Run Reward-Noises.exe (Program will run then close)
+ 4. Open config.json
+ 5. Populate config.json accordingly, check [Config.json Definitions](#Config.json-Example) for info on the config
+ 6.  Create corresponding rewards on [Twitch.tv dashboard](https://dashboard.twitch.tv/). **Ensure that reward name matches name defined in config.json**
+6. Populate audio directory with your audio files (must be .mp3 or .wav) **Ensure that file names match names defined in config.json**
+
+## Config.json Definitions
+#### app_auth
+This is a list which should contain your twitch application's ID and secret. If you haven't already made a twitch application it can be done on the [Twitch Developers](https://dev.twitch.tv/console/apps/create) site
+#### logins
+This is a list which should contain the Twitch usernames of the accounts you want this lister to listen on
+#### rewards
+This is a dictionary which should contain your rewards and they're desired audio files.
+Each key in the dict should be the exact name of the reward you want them to link to. The value for each key should be a list containing the names and the file extension of the files which you would like to be played when the reward is redeemed.
+If you provide multiple files within the value list the file played will be randomly chosen.
+
+    {"reward_name": ["audio_file_name.mp3"]}
+
+#### user_auth
+This is a list which will contain your user authentication. This is automatically filled out by the program. 
+### Config Example
+        {
+            "app_auth": [
+                "App_ID",
+                "App_Secret"
+            ],
+            "logins": ["sirspam_"],
+            "rewards": {
+                "Baka (Sound)": ["Aqua_Baka.mp3"],
+                "Nya (Sound)": [
+                    "Chocola_Nya.mp3",
+                    "Vanilla_Nya.wav"
+                ]
+            },
+            "user_auth": []
+        }
+###### *I initially wrote this in TypeScript but couldn't get audio working and I'm still upset about it >:(*
---
+++ program.py
@@ -0,0 +1,90 @@
+import logging
+from os import getcwd, mkdir, path
+from uuid import UUID
+from json import load, dump
+from random import choice
+
+from playsound import playsound
+
+from twitchAPI.pubsub import PubSub
+from twitchAPI.twitch import Twitch
+from twitchAPI.oauth import UserAuthenticator
+from twitchAPI.types import AuthScope, InvalidTokenException
+
+
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s: %(message)s', level=logging.INFO)
+
+cwd = getcwd()
+
+# Creates audio file if it doesn't already exist
+try:
+    mkdir(cwd+"\\audio")
+except FileExistsError:
+    pass
+
+# Loads config from config.json. Creates config.json if doesn't exist
+try:
+    config = (load(open("config.json",)))
+except FileNotFoundError:
+    logging.warning("config.json not found. Creating config")
+    dump({
+        "logins": list(),
+        "rewards": dict(),
+        "app_auth": ("App_ID","App_Secret"),
+        "user_auth": tuple()
+    }, open("config.json","w"), sort_keys=True, indent=4)
+    config = (load(open("config.json",)))
+    exit()
+
+# Checks app_auth has been filled out by user
+if config["app_auth"][0] == "App_ID" or config["app_auth"][0] == "App_Secret":
+    logging.error("app_auth hasn't been filled out! This can be done from the config.json file.\nIf you haven't created an application go to https://dev.twitch.tv/console")
+    exit()
+
+# Checks logins has been filled out by user
+if not config["logins"]:
+    logging.error("logins hasn't been filled out! This can be done from the config.json file.")
+    exit()
+
+# Checks all reward audio files are valid
+for reward in config["rewards"]:
+    for audio in config["rewards"][reward]:
+        if not path.exists(cwd+"\\audio\\"+audio):
+            logging.error(f"Audio file {audio} not found!\nMake sure it's in the audio file.")
+            exit()
+
+def callback_channel_points(uuid: UUID, data: dict):
+    for reward in config["rewards"]:
+        if data["data"]["redemption"]["reward"]["title"] == reward:
+            logging.info(f"{data['data']['redemption']['reward']['title']} redeemed")
+            audio = choice(config["rewards"][reward])
+            playsound(cwd+"\\audio\\"+audio, block=False)
+            logging.info(f"{audio} played")
+
+def user_authenticate():
+    auth = UserAuthenticator(twitch, [AuthScope.CHANNEL_READ_REDEMPTIONS], force_verify=False)
+    token, refresh_token = auth.authenticate()
+    config["user_auth"] = (token, refresh_token)
+    dump(config, open("config.json","w"), sort_keys=True, indent=4)
+
+# Setting up Authentication and getting user id
+#twitch = Twitch("go1pnso8u1bmq1mgvigphu1joulfrq", "f71eqie6rg4z8ia9llkrlcjjq7ihrv")
+twitch = Twitch(config["app_auth"][0], config["app_auth"][1])
+if not config["user_auth"]:
+    user_authenticate()
+try:
+    twitch.set_user_authentication(config["user_auth"][0], [AuthScope.CHANNEL_READ_REDEMPTIONS], config["user_auth"][1])
+except InvalidTokenException:
+    user_authenticate()
+    twitch.set_user_authentication(config["user_auth"][0], [AuthScope.CHANNEL_READ_REDEMPTIONS], config["user_auth"][1])
+
+user_id = twitch.get_users(logins=config["logins"])["data"][0]["id"]
+pubsub = PubSub(twitch)
+
+
+# starting up PubSub
+pubsub.start()
+uuid = pubsub.listen_channel_points(user_id,  callback_channel_points)
+# Likely a better way to block the program than this
+input('press ENTER to close...\n')
+pubsub.stop()
